ee1b4a08e326afe48d2735c31949e3a630117029
Le Vivilet <levivilet@pm.me>
2026-03-12 10:32:12 +0100

    fix

--- packages/e2e/src/chat-debug-view.show-input-events-toggle.ts
+++ packages/e2e/src/chat-debug-view.show-input-events-toggle.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.show-input-events-toggle'
 
-export const skip=1
+export const skip = 1
 
 export const test: Test = async ({ Command, expect, Locator }) => {
   // arrange
