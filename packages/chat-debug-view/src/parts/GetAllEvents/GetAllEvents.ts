55214fa51ce35553847826c76be9a686254bf929
Le Vivilet <levivilet@pm.me>
2026-03-12 10:43:30 +0100

    fix

--- packages/chat-debug-view/src/parts/GetAllEvents/GetAllEvents.ts
+++ packages/chat-debug-view/src/parts/GetAllEvents/GetAllEvents.ts
@@ -1,5 +1,5 @@
-import type { ChatViewEvent } from '../ChatViewEvent/ChatViewEvent.ts'
 import type { IDBPObjectStore } from 'idb'
+import type { ChatViewEvent } from '../ChatViewEvent/ChatViewEvent.ts'
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const getAllEvents = async (store: Pick<IDBPObjectStore, 'getAll'>): Promise<readonly ChatViewEvent[]> => {
