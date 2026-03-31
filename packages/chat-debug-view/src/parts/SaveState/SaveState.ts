24755fc2701eea79391250e50bdf11ae77f0bd68
Le Vivilet <levivilet@pm.me>
2026-03-31 15:54:53 +0200

    fix

--- packages/chat-debug-view/src/parts/SaveState/SaveState.ts
+++ packages/chat-debug-view/src/parts/SaveState/SaveState.ts
@@ -2,7 +2,8 @@ import type { SavedState } from '../SavedState/SavedState.ts'
 import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
 
 export const saveState = (state: ChatDebugViewState): SavedState => {
-  const { filterValue, height, sessionId, showEventStreamFinishedEvents, showInputEvents, showResponsePartEvents, useDevtoolsLayout, width, x, y } = state
+  const { filterValue, height, sessionId, showEventStreamFinishedEvents, showInputEvents, showResponsePartEvents, useDevtoolsLayout, width, x, y } =
+    state
   return {
     filterValue,
     height,
