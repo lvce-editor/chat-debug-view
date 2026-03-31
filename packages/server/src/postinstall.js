65af415f258a7a0c601f111f4a26725b7c10e421
Le Vivilet <levivilet@pm.me>
2026-03-31 16:09:38 +0200

    fix

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -26,7 +26,6 @@ const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'r
 
 const content = await readFile(rendererWorkerMainPath, 'utf-8')
 
-const chatViewWorkerPath = join(root, '.tmp/dist/dist/chatViewWorkerMain.js')
 const chatDebugViewWorkerPath = join(root, '.tmp/dist-chat-debug-view/dist/chatDebugViewWorkerMain.js')
 
 const replaceWorkerUrl = (currentContent, variableName, packageName, workerMainName, localPath) => {
@@ -41,7 +40,6 @@ const ${variableName} = \`${remoteUrl}\``
 }
 
 let newContent = content
-newContent = replaceWorkerUrl(newContent, 'chatViewWorkerUrl', 'chat-view', 'chatViewWorkerMain.js', chatViewWorkerPath)
 newContent = replaceWorkerUrl(newContent, 'chatDebugViewWorkerUrl', 'chat-debug-view', 'chatDebugViewWorkerMain.js', chatDebugViewWorkerPath)
 
 if (newContent !== content) {
