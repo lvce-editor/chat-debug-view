76a225bbddd7499fa307f817161a6a41a35ce5ee
Le Vivilet <levivilet@pm.me>
2026-04-01 22:19:07 +0200

    fix

--- packages/e2e/src/chat-debug-view.indexed-db-not-supported.ts
+++ packages/e2e/src/chat-debug-view.indexed-db-not-supported.ts
@@ -2,22 +2,14 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.indexed-db-not-supported'
 
-<<<<<<< HEAD
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, Locator }) => {
   await Command.execute('Main.openUri', 'chat-debug://e2e-session-indexed-db-not-supported')
   try {
     // act
     await Command.execute('ChatDebug.setIndexedDbSupportForTest', false)
     await Command.execute('ChatDebug.refresh')
-=======
-export const skip = 1
-
-export const test: Test = async ({ Command, expect, Locator }) => {
-  await Command.execute('ChatDebug.setIndexedDbSupportForTest', false)
-  try {
-    // act
-    await Command.execute('Main.openUri', 'chat-debug://e2e-session-indexed-db-not-supported')
->>>>>>> origin/main
 
     // assert
     const errorMessage = Locator('.ChatDebugViewError')
