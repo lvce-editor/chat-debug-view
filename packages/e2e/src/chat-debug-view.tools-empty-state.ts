56165cf59d83b438cb0a8ac2502d38e5b714ecbb
Le Vivilet <levivilet@pm.me>
2026-04-09 17:00:16 +0200

    feature: remove skip export from chat-debug-view.tools-empty-state test

--- packages/e2e/src/chat-debug-view.tools-empty-state.ts
+++ packages/e2e/src/chat-debug-view.tools-empty-state.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.tools-empty-state'
 
-export const skip = 1
-
 export const test: Test = async ({ ChatDebug, expect, Locator }) => {
   // arrange
   await ChatDebug.open('e2e-session-tools-empty-state')
