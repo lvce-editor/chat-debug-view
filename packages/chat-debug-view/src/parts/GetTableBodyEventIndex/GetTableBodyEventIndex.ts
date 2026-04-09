6a683589dcebaf11b1303ae9ccd2b6488f02ae86
Le Vivilet <levivilet@pm.me>
2026-04-09 22:16:36 +0200

    fix

--- packages/chat-debug-view/src/parts/GetTableBodyEventIndex/GetTableBodyEventIndex.ts
+++ packages/chat-debug-view/src/parts/GetTableBodyEventIndex/GetTableBodyEventIndex.ts
@@ -8,11 +8,8 @@ const devtoolsTimelineHeight = 88
 const devtoolsTableHeaderHeight = 24
 const devtoolsTableRowHeight = 24
 
-<<<<<<< HEAD
-=======
 export const MenuChatDebugTableBody = 2190
 
->>>>>>> origin/main
 const getTableBodyY = (state: ChatDebugViewState, hasTimeline: boolean): number => {
   return state.y + viewPadding + devtoolsTopHeight + devtoolsRootGap + (hasTimeline ? devtoolsTimelineHeight : 0) + devtoolsTableHeaderHeight
 }
