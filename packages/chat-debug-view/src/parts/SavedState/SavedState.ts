29da7f577540cc33f63230b29f577efb152e6e32
Le Vivilet <levivilet@pm.me>
2026-04-09 16:28:02 +0200

    fix

--- packages/chat-debug-view/src/parts/SavedState/SavedState.ts
+++ packages/chat-debug-view/src/parts/SavedState/SavedState.ts
@@ -1,9 +1,9 @@
 export interface SavedState {
   readonly eventCategoryFilter: string
   readonly filterValue: string
+  readonly selectedDetailTab: string
   readonly selectedEventId: number | null
   readonly sessionId: string
-  readonly selectedDetailTab: string
   readonly timelineEndSeconds: string
   readonly timelineStartSeconds: string
 }
