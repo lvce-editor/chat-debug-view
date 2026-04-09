65ce667de106646d823c15b77111e3a7b482f5eb
Le Vivilet <levivilet@pm.me>
2026-04-09 14:22:22 +0200

    fix

--- packages/chat-debug-view/test/RenderEventListeners.test.ts
+++ packages/chat-debug-view/test/RenderEventListeners.test.ts
@@ -1,6 +1,4 @@
 import { expect, test } from '@jest/globals'
-import { EventExpression } from '@lvce-editor/constants'
-import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
 test('renderEventListeners should register delegated row click with dataset index', () => {
