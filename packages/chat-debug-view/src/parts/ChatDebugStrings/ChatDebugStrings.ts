828adf4e955e327ae5e4b7898f26bf5422bd4973
Le Vivilet <levivilet@pm.me>
2026-04-09 22:00:42 +0200

    feature: add localization strings for chat debug view

---
+++ packages/chat-debug-view/src/parts/ChatDebugStrings/ChatDebugStrings.ts
@@ -0,0 +1,250 @@
+import * as I18nString from '@lvce-editor/i18n'
+import * as UiStrings from '../UiStrings/UiStrings.ts'
+
+export const installation = (): string => {
+  return I18nString.i18nString(UiStrings.Installation)
+}
+
+export const marketplace = (): string => {
+  return I18nString.i18nString(UiStrings.Marketplace)
+}
+
+export const categories = (): string => {
+  return I18nString.i18nString(UiStrings.Categories)
+}
+
+export const copyLink = (): string => {
+  return I18nString.i18nString(UiStrings.CopyLink)
+}
+
+export const resources = (): string => {
+  return I18nString.i18nString(UiStrings.Resources)
+}
+
+export const copy = (): string => {
+  return I18nString.i18nString(UiStrings.Copy)
+}
+
+export const copyImage = (): string => {
+  return I18nString.i18nString(UiStrings.CopyImage)
+}
+
+export const copyImageUrl = (): string => {
+  return I18nString.i18nString(UiStrings.CopyImageUrl)
+}
+
+export const changelog = (): string => {
+  return I18nString.i18nString(UiStrings.Changelog)
+}
+
+export const details = (): string => {
+  return I18nString.i18nString(UiStrings.Details)
+}
+
+export const disable = (): string => {
+  return I18nString.i18nString(UiStrings.Disable)
+}
+
+export const features = (): string => {
+  return I18nString.i18nString(UiStrings.Features)
+}
+
+export const none = (): string => {
+  return I18nString.i18nString(UiStrings.None)
+}
+
+export const openInNewTab = (): string => {
+  return I18nString.i18nString(UiStrings.OpenInNewTab)
+}
+
+export const label = (): string => {
+  return I18nString.i18nString(UiStrings.Label)
+}
+
+export const openImageInNewTab = (): string => {
+  return I18nString.i18nString(UiStrings.OpenImageInNewTab)
+}
+
+export const saveImageAs = (): string => {
+  return I18nString.i18nString(UiStrings.SaveImageAs)
+}
+
+export const fileMatch = (): string => {
+  return I18nString.i18nString(UiStrings.FileMatch)
+}
+
+export const schema = (): string => {
+  return I18nString.i18nString(UiStrings.Schema)
+}
+
+export const status = (): string => {
+  return I18nString.i18nString(UiStrings.Status)
+}
+
+export const setColorTheme = (): string => {
+  return I18nString.i18nString(UiStrings.SetColorTheme)
+}
+
+export const enable = (): string => {
+  return I18nString.i18nString(UiStrings.Enable)
+}
+
+export const theme = (): string => {
+  return I18nString.i18nString(UiStrings.Theme)
+}
+
+export const commands = (): string => {
+  return I18nString.i18nString(UiStrings.Commands)
+}
+
+export const webViews = (): string => {
+  return I18nString.i18nString(UiStrings.WebViews)
+}
+
+export const activationEvents = (): string => {
+  return I18nString.i18nString(UiStrings.ActivationEvents)
+}
+
+export const runtimeStatus = (): string => {
+  return I18nString.i18nString(UiStrings.RuntimeStatus)
+}
+
+export const jsonValidation = (): string => {
+  return I18nString.i18nString(UiStrings.JsonValidation)
+}
+
+export const programmingLanguages = (): string => {
+  return I18nString.i18nString(UiStrings.ProgrammingLanguages)
+}
+
+export const settings = (): string => {
+  return I18nString.i18nString(UiStrings.Settings)
+}
+
+export const id = (): string => {
+  return I18nString.i18nString(UiStrings.Id)
+}
+
+export const name = (): string => {
+  return I18nString.i18nString(UiStrings.Name)
+}
+
+export const fileExtensions = (): string => {
+  return I18nString.i18nString(UiStrings.FileExtensions)
+}
+
+export const grammar = (): string => {
+  return I18nString.i18nString(UiStrings.Grammar)
+}
+
+export const snippets = (): string => {
+  return I18nString.i18nString(UiStrings.Snippets)
+}
+
+export const selector = (): string => {
+  return I18nString.i18nString(UiStrings.Selector)
+}
+
+export const contentSecurityPolicy = (): string => {
+  return I18nString.i18nString(UiStrings.ContentSecurityPolicy)
+}
+
+export const elements = (): string => {
+  return I18nString.i18nString(UiStrings.Elements)
+}
+
+export const notImplemented = (): string => {
+  return I18nString.i18nString(UiStrings.NotImplemented)
+}
+
+export const uninstall = (): string => {
+  return I18nString.i18nString(UiStrings.Uninstall)
+}
+
+export const scrollToTop = (): string => {
+  return I18nString.i18nString(UiStrings.ScrollToTop)
+}
+
+export const noReadmeFound = (): string => {
+  return I18nString.i18nString(UiStrings.NoReadmeFound)
+}
+
+export const emptyCommandsArray = (): string => {
+  return I18nString.i18nString(UiStrings.EmptyCommandsArray)
+}
+
+export const importTime = (): string => {
+  return I18nString.i18nString(UiStrings.ImportTime)
+}
+
+export const activationTime = (): string => {
+  return I18nString.i18nString(UiStrings.ActivationTime)
+}
+
+export const unsupportedFeature = (): string => {
+  return I18nString.i18nString(UiStrings.UnsupportedFeature)
+}
+
+export const selectedFeatureUnknownOrUnsupported = (): string => {
+  return I18nString.i18nString(UiStrings.SelectedFeatureUnknownOrUnsupported)
+}
+
+export const identifier = (): string => {
+  return I18nString.i18nString(UiStrings.Identifier)
+}
+
+export const version = (): string => {
+  return I18nString.i18nString(UiStrings.Version)
+}
+
+export const lastUpdated = (): string => {
+  return I18nString.i18nString(UiStrings.LastUpdated)
+}
+
+export const size = (): string => {
+  return I18nString.i18nString(UiStrings.Size)
+}
+
+export const published = (): string => {
+  return I18nString.i18nString(UiStrings.Published)
+}
+
+export const lastReleased = (): string => {
+  return I18nString.i18nString(UiStrings.LastReleased)
+}
+
+export const issues = (): string => {
+  return I18nString.i18nString(UiStrings.Issues)
+}
+
+export const repository = (): string => {
+  return I18nString.i18nString(UiStrings.Repository)
+}
+
+export const license = (): string => {
+  return I18nString.i18nString(UiStrings.License)
+}
+
+export const downloadCount = (): string => {
+  return I18nString.i18nString(UiStrings.DownloadCount)
+}
+
+export const rating = (): string => {
+  return I18nString.i18nString(UiStrings.Rating)
+}
+
+export const propertyMustBeOfTypeString = (): string => {
+  return I18nString.i18nString(UiStrings.PropertyMustBeOfTypeString)
+}
+
+export const stringMustNotBeEmpty = (): string => {
+  return I18nString.i18nString(UiStrings.StringMustNotBeEmpty)
+}
+
+export const schemaNotFound = (): string => {
+  return I18nString.i18nString(UiStrings.SchemaNotFound)
+}
+
+export const invalidLink = (): string => {
+  return I18nString.i18nString(UiStrings.InvalidLink)
+}
