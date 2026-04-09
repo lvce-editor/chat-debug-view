36750e9c57945f4d5a60280b396b766c911c5b17
Le Vivilet <levivilet@pm.me>
2026-04-09 16:30:08 +0200

    unused

--- packages/chat-debug-view/src/parts/EventStableIdsState/EventStableIdsState.ts
+++
@@ -1,7 +0,0 @@
-import type { ChatViewEvent } from '../ChatViewEvent/ChatViewEvent.ts'
-
-export const eventStableIds = new WeakMap<ChatViewEvent, string>()
-
-export const eventStableIdState = {
-  nextStableEventId: 1,
-}
