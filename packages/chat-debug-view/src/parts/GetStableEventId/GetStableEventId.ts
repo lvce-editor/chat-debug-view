a3e006b575fa90a1c543c6c16a6d3a58d524222f
Le Vivilet <levivilet@pm.me>
2026-04-09 16:30:32 +0200

    fix

--- packages/chat-debug-view/src/parts/GetStableEventId/GetStableEventId.ts
+++
@@ -1,6 +0,0 @@
-import type { ChatViewEvent } from '../ChatViewEvent/ChatViewEvent.ts'
-import { getOrCreateStableEventId } from '../GetOrCreateStableEventId/GetOrCreateStableEventId.ts'
-
-export const getStableEventId = (event: ChatViewEvent): string => {
-  return getOrCreateStableEventId(event)
-}
