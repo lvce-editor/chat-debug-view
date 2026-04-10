5984165fdadcac9a21566e11578e84b9b98347ca
Le Vivilet <levivilet@pm.me>
2026-04-10 13:16:00 +0200

    refactor: implement lazy initialization for timestamp formatter

--- packages/chat-debug-view/src/parts/FormatTimestamp/FormatTimestamp.ts
+++ packages/chat-debug-view/src/parts/FormatTimestamp/FormatTimestamp.ts
@@ -1,15 +1,23 @@
-const timestampFormatter = new Intl.DateTimeFormat('en-US', {
-  day: '2-digit',
-  fractionalSecondDigits: 3,
-  hour: '2-digit',
-  hourCycle: 'h23',
-  minute: '2-digit',
-  month: 'short',
-  second: '2-digit',
-  timeZone: 'UTC',
-  year: 'numeric',
-})
+let timestampFormatter: Intl.DateTimeFormat | undefined
+
+const getTimestampFormatter = (): Intl.DateTimeFormat => {
+  if (!timestampFormatter) {
+    timestampFormatter = new Intl.DateTimeFormat('en-US', {
+      day: '2-digit',
+      fractionalSecondDigits: 3,
+      hour: '2-digit',
+      hourCycle: 'h23',
+      minute: '2-digit',
+      month: 'short',
+      second: '2-digit',
+      timeZone: 'UTC',
+      year: 'numeric',
+    })
+  }
+
+  return timestampFormatter
+}
 
 export const formatTimestamp = (date: Readonly<Date>): string => {
-  return `${timestampFormatter.format(date)} UTC`
+  return `${getTimestampFormatter().format(date)} UTC`
 }
