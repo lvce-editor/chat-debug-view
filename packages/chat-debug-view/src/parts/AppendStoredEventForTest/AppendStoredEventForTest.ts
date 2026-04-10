3e75cf14dd1901cf692fe6478dc4fee4b988255e
Le Vivilet <levivilet@pm.me>
2026-04-10 11:25:16 +0200

    refactor: simplify event appending logic in appendStoredEventForTest

--- packages/chat-debug-view/src/parts/AppendStoredEventForTest/AppendStoredEventForTest.ts
+++ packages/chat-debug-view/src/parts/AppendStoredEventForTest/AppendStoredEventForTest.ts
@@ -2,11 +2,7 @@ import type { ChatViewEvent } from '../ChatViewEvent/ChatViewEvent.ts'
 import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
 import * as ChatStorageWorkerClient from '../ChatStorageWorkerClient/ChatStorageWorkerClient.ts'
 
-export const appendStoredEventForTestDependencies = {
-  appendEvent: ChatStorageWorkerClient.appendEvent,
-}
-
 export const appendStoredEventForTest = async (state: ChatDebugViewState, event: ChatViewEvent): Promise<ChatDebugViewState> => {
-  await appendStoredEventForTestDependencies.appendEvent(event)
+  await ChatStorageWorkerClient.appendEvent(event)
   return state
 }
