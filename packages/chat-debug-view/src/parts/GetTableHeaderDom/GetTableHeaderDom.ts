25a7d99a34bbf572147c0959de8146550b3f012c
Le Vivilet <levivilet@pm.me>
2026-04-10 13:22:25 +0200

    feature: extract getHeaderCellNodes function and add corresponding tests

--- packages/chat-debug-view/src/parts/GetTableHeaderDom/GetTableHeaderDom.ts
+++ packages/chat-debug-view/src/parts/GetTableHeaderDom/GetTableHeaderDom.ts
@@ -1,48 +1,8 @@
-import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
-import * as ChatDebugStrings from '../ChatDebugStrings/ChatDebugStrings.ts'
-import { ChatDebugViewHeaderCell, ChatDebugViewTableHeader, ChatDebugViewTableHeaderRow } from '../ClassNames/ClassNames.ts'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { ChatDebugViewTableHeader, ChatDebugViewTableHeaderRow } from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { getHeaderCellNodes } from '../GetHeaderCellNodes/GetHeaderCellNodes.ts'
 import { defaultVisibleTableColumns } from '../TableColumn/TableColumn.ts'
-import * as TableColumn from '../TableColumn/TableColumn.ts'
-
-const getHeaderCellNodes = (visibleTableColumns: readonly string[]): readonly VirtualDomNode[] => {
-  return visibleTableColumns.flatMap((column) => {
-    switch (column) {
-      case TableColumn.Duration:
-        return [
-          {
-            childCount: 1,
-            className: ChatDebugViewHeaderCell,
-            scope: 'col',
-            type: VirtualDomElements.Th,
-          },
-          text(ChatDebugStrings.duration()),
-        ]
-      case TableColumn.Status:
-        return [
-          {
-            childCount: 1,
-            className: ChatDebugViewHeaderCell,
-            scope: 'col',
-            type: VirtualDomElements.Th,
-          },
-          text(ChatDebugStrings.status()),
-        ]
-      case TableColumn.Type:
-        return [
-          {
-            childCount: 1,
-            className: ChatDebugViewHeaderCell,
-            scope: 'col',
-            type: VirtualDomElements.Th,
-          },
-          text(ChatDebugStrings.type()),
-        ]
-      default:
-        return []
-    }
-  })
-}
 
 export const getTableHeaderDom = (visibleTableColumns: readonly string[] = defaultVisibleTableColumns): readonly VirtualDomNode[] => {
   const headerCellNodes = getHeaderCellNodes(visibleTableColumns)
---
+++ packages/chat-debug-view/test/GetHeaderCellNodes.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@jest/globals'
+import { VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import * as GetHeaderCellNodes from '../src/parts/GetHeaderCellNodes/GetHeaderCellNodes.ts'
+import * as TableColumn from '../src/parts/TableColumn/TableColumn.ts'
+
+test('getHeaderCellNodes should render the visible header cells', () => {
+  const dom = GetHeaderCellNodes.getHeaderCellNodes([TableColumn.Type, TableColumn.Status]) as readonly {
+    readonly childCount?: number
+    readonly className?: string
+    readonly scope?: string
+  }[]
+
+  expect(dom).toEqual([
+    {
+      childCount: 1,
+      className: 'ChatDebugViewHeaderCell',
+      scope: 'col',
+      type: VirtualDomElements.Th,
+    },
+    text('Type'),
+    {
+      childCount: 1,
+      className: 'ChatDebugViewHeaderCell',
+      scope: 'col',
+      type: VirtualDomElements.Th,
+    },
+    text('Status'),
+  ])
+})
