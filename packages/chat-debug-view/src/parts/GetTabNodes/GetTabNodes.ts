d0ae697dcae5a7b88417acad656e5b78c4e8fb6d
Le Vivilet <levivilet@pm.me>
2026-04-10 13:25:52 +0200

    refactor: extract getDetailTabDom function for improved readability in getTabNodes (#135)

--- packages/chat-debug-view/src/parts/GetTabNodes/GetTabNodes.ts
+++ packages/chat-debug-view/src/parts/GetTabNodes/GetTabNodes.ts
@@ -6,25 +6,29 @@ import { getPanelId } from '../GetPanelId/GetPanelId.ts'
 import { getTabId } from '../GetTabId/GetTabId.ts'
 import * as InputName from '../InputName/InputName.ts'
 
+const getDetailTabDom = (detailTab: string, selectedDetailTab: string): readonly VirtualDomNode[] => {
+  const isSelected = detailTab === selectedDetailTab
+  return [
+    {
+      'aria-controls': getPanelId(detailTab),
+      'aria-selected': isSelected,
+      childCount: 1,
+      className: mergeClassNames(ChatDebugViewDetailsTab, isSelected ? ChatDebugViewDetailsTabSelected : ''),
+      id: getTabId(detailTab),
+      name: InputName.DetailTab,
+      onChange: DomEventListenerFunctions.SelectDetailTab,
+      onClick: DomEventListenerFunctions.SelectDetailTab,
+      role: 'tab',
+      tabIndex: isSelected ? 0 : -1,
+      type: VirtualDomElements.Button,
+      value: detailTab,
+    },
+    text(DetailTab.getDetailTabLabel(detailTab)),
+  ]
+}
+
 export const getTabNodes = (selectedDetailTab: string): readonly VirtualDomNode[] => {
   return DetailTab.detailTabs.flatMap((detailTab) => {
-    const isSelected = detailTab === selectedDetailTab
-    return [
-      {
-        'aria-controls': getPanelId(detailTab),
-        'aria-selected': isSelected,
-        childCount: 1,
-        className: mergeClassNames(ChatDebugViewDetailsTab, isSelected ? ChatDebugViewDetailsTabSelected : ''),
-        id: getTabId(detailTab),
-        name: InputName.DetailTab,
-        onChange: DomEventListenerFunctions.SelectDetailTab,
-        onClick: DomEventListenerFunctions.SelectDetailTab,
-        role: 'tab',
-        tabIndex: isSelected ? 0 : -1,
-        type: VirtualDomElements.Button,
-        value: detailTab,
-      },
-      text(DetailTab.getDetailTabLabel(detailTab)),
-    ]
+    return getDetailTabDom(detailTab, selectedDetailTab)
   })
 }
