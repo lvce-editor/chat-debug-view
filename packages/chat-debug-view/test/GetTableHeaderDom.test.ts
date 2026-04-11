3d9ebb725d9db5ee9ce30564e98a86c3625dd4e5
Le Vivilet <levivilet@pm.me>
2026-04-11 22:23:16 +0200

    fix

--- packages/chat-debug-view/test/GetTableHeaderDom.test.ts
+++ packages/chat-debug-view/test/GetTableHeaderDom.test.ts
@@ -75,187 +75,38 @@ test('getTableHeaderDom should omit hidden columns', () => {
     {
       childCount: 1,
       className: 'TableCell',
-      name: TableColumn.Type,
-      onClick: handleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text('Type'),
-    {
-      childCount: 1,
-      className: 'TableCell',
-      name: TableColumn.Status,
-      onClick: handleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text('Status'),
-  ])
-})
+  test('getTableDom should render header and body nodes for the table', () => {
+    const rowNodes = [
+      {
+        childCount: 0,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+    ]
+    const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1) as readonly {
+      readonly childCount?: number
+      readonly className?: string
+      readonly onContextMenu?: number
+      readonly onPointerDown?: number
+      readonly role?: string
+      readonly scope?: string
+      readonly text?: string
+      readonly type?: number
+    }[]
 
-      name: TableColumn.Duration,
-  const rowNodes = [
-    {
-      childCount: 0,
-    text('Time'),
-      type: VirtualDomElements.Tr,
-    },
-  ]
-  const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1) as readonly {
-    readonly childCount?: number
-    readonly className?: string
-    readonly onPointerDown?: number
-    readonly onContextMenu?: number
-    readonly scope?: string
-  }[]
-
-  expect(dom).toEqual([
-    {
-      childCount: 2,
-      className: 'TableWrapper',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 3,
-      className: 'Table',
-      type: VirtualDomElements.Table,
-    },
-    {
-      childCount: 3,
-      className: 'ColGroup',
-      type: VirtualDomElements.ColGroup,
-    },
-    {
-      childCount: 0,
-      className: 'TableCol TableColZero',
-      type: VirtualDomElements.Col,
-    },
-    {
-      childCount: 0,
-      className: 'TableCol TableColOne',
-      type: VirtualDomElements.Col,
-    },
-    {
-      childCount: 0,
-      className: 'TableCol TableColTwo',
-      type: VirtualDomElements.Col,
-    },
-    {
-      childCount: 1,
-      className: 'TableHead',
-      onContextMenu: DomEventListenerFunctions.HandleHeaderContextMenu,
-      type: VirtualDomElements.THead,
-    },
-    {
-      childCount: 3,
-      className: 'TableRow',
-      type: VirtualDomElements.Tr,
-    },
-    {
-      childCount: 1,
-      className: 'TableCell',
-      name: TableColumn.Type,
-      onClick: handleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text('Type'),
-    {
-      childCount: 1,
-      className: 'TableCell',
-      name: TableColumn.Duration,
-      onClick: handleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text('Time'),
-    {
-      childCount: 1,
-      className: 'TableCell',
-      name: TableColumn.Status,
-      onClick: handleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text('Status'),
-    {
-      childCount: 1,
-      className: 'TableBody',
-      onContextMenu: DomEventListenerFunctions.HandleTableBodyContextMenu,
-      onPointerDown: DomEventListenerFunctions.HandleEventRowClickAt,
-      type: VirtualDomElements.TBody,
-    },
-    {
-      childCount: 0,
-      className: 'TableRow',
-      type: VirtualDomElements.Tr,
-    },
-    {
-      childCount: 3,
-      className: 'Resizers',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      className: 'Resizer ResizerOne',
-      name: 'ResizerOne',
-      onPointerDown: DomEventListenerFunctions.HandleTableResizerPointerDown,
-      role: 'none',
-      type: VirtualDomElements.Button,
-    },
-    {
-      childCount: 0,
-      className: 'ResizerInner',
-      role: 'none',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      className: 'Resizer ResizerTwo',
-      name: 'ResizerTwo',
-      onPointerDown: DomEventListenerFunctions.HandleTableResizerPointerDown,
-      role: 'none',
-      type: VirtualDomElements.Button,
-    },
-    {
-      childCount: 0,
-      className: 'ResizerInner',
-      role: 'none',
-      type: VirtualDomElements.Div,
-    },
-  ])
-})
-
-test('getTableDom should count only direct children for the resizer wrapper', () => {
-  const rowNodes = [
-    {
-      childCount: 0,
-      className: 'TableRow',
-      type: VirtualDomElements.Tr,
-    },
-  ]
-  const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1) as readonly {
-    readonly childCount?: number
-    readonly className?: string
-  }[]
-  const resizers = dom.find((node) => node.className === 'Resizers')
-
-  expect(resizers?.childCount).toBe(3)
-})
-
-test('getTableDom should omit hidden columns from the colgroup', () => {
-  const rowNodes = [
-    {
-      childCount: 0,
-      className: 'TableRow',
-      type: VirtualDomElements.Tr,
-    },
-  ]
-  const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1, [TableColumn.Type, TableColumn.Status]) as readonly {
-    readonly childCount?: number
-    readonly className?: string
-    readonly type?: number
-  }[]
-
-  expect(dom).toEqual(
-    expect.arrayContaining([
+    expect(dom).toEqual([
       {
         childCount: 2,
+        className: 'TableWrapper',
+        type: VirtualDomElements.Div,
+      },
+      {
+        childCount: 3,
+        className: 'Table',
+        type: VirtualDomElements.Table,
+      },
+      {
+        childCount: 3,
         className: 'ColGroup',
         type: VirtualDomElements.ColGroup,
       },
@@ -269,44 +120,196 @@ test('getTableDom should omit hidden columns from the colgroup', () => {
         className: 'TableCol TableColOne',
         type: VirtualDomElements.Col,
       },
-    ]),
-  )
-  expect(dom).not.toEqual(
-    expect.arrayContaining([
       {
+        childCount: 0,
         className: 'TableCol TableColTwo',
         type: VirtualDomElements.Col,
       },
-    ]),
-  )
-})
+      {
+        childCount: 1,
+        className: 'TableHead',
+        onContextMenu: DomEventListenerFunctions.HandleHeaderContextMenu,
+        type: VirtualDomElements.THead,
+      },
+      {
+        childCount: 3,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+      {
+        childCount: 1,
+        className: 'TableCell',
+        name: TableColumn.Type,
+        onClick: handleTableHeaderClick,
+        type: VirtualDomElements.Th,
+      },
+      text('Type'),
+      {
+        childCount: 1,
+        className: 'TableCell',
+        name: TableColumn.Duration,
+        onClick: handleTableHeaderClick,
+        type: VirtualDomElements.Th,
+      },
+      text('Time'),
+      {
+        childCount: 1,
+        className: 'TableCell',
+        name: TableColumn.Status,
+        onClick: handleTableHeaderClick,
+        type: VirtualDomElements.Th,
+      },
+      text('Status'),
+      {
+        childCount: 1,
+        className: 'TableBody',
+        onContextMenu: DomEventListenerFunctions.HandleTableBodyContextMenu,
+        onPointerDown: DomEventListenerFunctions.HandleEventRowClickAt,
+        type: VirtualDomElements.TBody,
+      },
+      {
+        childCount: 0,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+      {
+        childCount: 3,
+        className: 'Resizers',
+        type: VirtualDomElements.Div,
+      },
+      {
+        childCount: 1,
+        className: 'Resizer ResizerOne',
+        name: 'ResizerOne',
+        onPointerDown: DomEventListenerFunctions.HandleTableResizerPointerDown,
+        role: 'none',
+        type: VirtualDomElements.Button,
+      },
+      {
+        childCount: 0,
+        className: 'ResizerInner',
+        role: 'none',
+        type: VirtualDomElements.Div,
+      },
+      {
+        childCount: 1,
+        className: 'Resizer ResizerTwo',
+        name: 'ResizerTwo',
+        onPointerDown: DomEventListenerFunctions.HandleTableResizerPointerDown,
+        role: 'none',
+        type: VirtualDomElements.Button,
+      },
+      {
+        childCount: 0,
+        className: 'ResizerInner',
+        role: 'none',
+        type: VirtualDomElements.Div,
+      },
+    ])
+  })
 
-test('getTableDom should render summary status below the table when provided', () => {
-  const rowNodes = [
-    {
-      childCount: 0,
-      className: 'TableRow',
-      type: VirtualDomElements.Tr,
-    },
-  ]
-  const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1, undefined, undefined, '1 event, 0ms from start to finish') as readonly {
-    readonly childCount?: number
-    readonly className?: string
-    readonly role?: string
-    readonly text?: string
-    readonly type?: number
-  }[]
-  const tableWrapperIndex = dom.findIndex((node) => node.className === 'TableWrapper')
-  const summaryIndex = dom.findIndex((node) => node.className === 'TableSummary')
-  const tableWrapper = dom[tableWrapperIndex]
+  test('getTableDom should count only direct children for the resizer wrapper', () => {
+    const rowNodes = [
+      {
+        childCount: 0,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+    ]
+    const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1) as readonly {
+      readonly childCount?: number
+      readonly className?: string
+    }[]
+    const resizers = dom.find((node) => node.className === 'Resizers')
 
-  expect(tableWrapper).toEqual(
-    expect.objectContaining({
-      childCount: 2,
-      className: 'TableWrapper',
-      type: VirtualDomElements.Div,
-    }),
-  )
+    expect(resizers?.childCount).toBe(3)
+  })
+
+  test('getTableDom should omit hidden columns from the colgroup', () => {
+    const rowNodes = [
+      {
+        childCount: 0,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+    ]
+    const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1, [TableColumn.Type, TableColumn.Status]) as readonly {
+      readonly childCount?: number
+      readonly className?: string
+      readonly type?: number
+    }[]
+
+    expect(dom).toEqual(
+      expect.arrayContaining([
+        {
+          childCount: 2,
+          className: 'ColGroup',
+          type: VirtualDomElements.ColGroup,
+        },
+        {
+          childCount: 0,
+          className: 'TableCol TableColZero',
+          type: VirtualDomElements.Col,
+        },
+        {
+          childCount: 0,
+          className: 'TableCol TableColOne',
+          type: VirtualDomElements.Col,
+        },
+      ]),
+    )
+    expect(dom).not.toEqual(
+      expect.arrayContaining([
+        {
+          className: 'TableCol TableColTwo',
+          type: VirtualDomElements.Col,
+        },
+      ]),
+    )
+  })
+
+  test('getTableDom should render summary status below the table when provided', () => {
+    const rowNodes = [
+      {
+        childCount: 0,
+        className: 'TableRow',
+        type: VirtualDomElements.Tr,
+      },
+    ]
+    const dom = GetTableDom.getTableDom(rowNodes as readonly any[], 1, undefined, undefined, '1 event, 0ms from start to finish') as readonly {
+      readonly childCount?: number
+      readonly className?: string
+      readonly role?: string
+      readonly text?: string
+      readonly type?: number
+    }[]
+    const tableWrapperIndex = dom.findIndex((node) => node.className === 'TableWrapper')
+    const summaryIndex = dom.findIndex((node) => node.className === 'TableSummary')
+    const tableWrapper = dom[tableWrapperIndex]
+
+    expect(tableWrapper).toEqual(
+      expect.objectContaining({
+        childCount: 2,
+        className: 'TableWrapper',
+        type: VirtualDomElements.Div,
+      }),
+    )
+    expect(summaryIndex).toBeGreaterThan(tableWrapperIndex)
+    expect(dom).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          childCount: 1,
+          className: 'TableSummary',
+          role: 'status',
+          type: VirtualDomElements.Div,
+        }),
+        expect.objectContaining({
+          text: '1 event, 0ms from start to finish',
+          type: 12,
+        }),
+      ]),
+    )
+  })
   expect(summaryIndex).toBeGreaterThan(tableWrapperIndex)
   expect(dom).toEqual(
     expect.arrayContaining([
