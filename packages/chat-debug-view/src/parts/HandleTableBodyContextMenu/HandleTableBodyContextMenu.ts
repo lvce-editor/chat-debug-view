8939aa6c258cd1003cc683d0b75aef42ca4a7a27
Le Vivilet <levivilet@pm.me>
2026-04-11 08:58:09 +0200

    args

--- packages/chat-debug-view/src/parts/HandleTableBodyContextMenu/HandleTableBodyContextMenu.ts
+++ packages/chat-debug-view/src/parts/HandleTableBodyContextMenu/HandleTableBodyContextMenu.ts
@@ -1,12 +1,12 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
+import { show2 } from '../ContextMenu/ContextMenu.ts'
 import { getTableBodyEventIndex } from '../GetTableBodyEventIndex/GetTableBodyEventIndex.ts'
 
 export const MenuChatDebugTableBody = 2190
 
 export const handleTableBodyContextMenu = async (state: ChatDebugViewState, eventX: number, eventY: number): Promise<ChatDebugViewState> => {
   const eventIndex = getTableBodyEventIndex(state, eventX, eventY)
-  await RendererWorker.showContextMenu2(state.uid, MenuChatDebugTableBody, eventX, eventY, {
+  await show2(state.uid, MenuChatDebugTableBody, eventX, eventY, {
     eventIndex,
     menuId: MenuChatDebugTableBody,
   })
