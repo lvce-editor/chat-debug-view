7db5c5ba2170c81013cc560a6ebe764d44fe84ac
Le Vivilet <levivilet@pm.me>
2026-04-11 17:17:53 +0200

    skip

--- packages/e2e/src/chat-debug-view.table-sort.ts
+++ packages/e2e/src/chat-debug-view.table-sort.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.table-sort'
 
+export const skip = 1
+
 export const test: Test = async ({ ChatDebug, expect, Locator }) => {
   await ChatDebug.open('e2e-session-table-sort')
   await expect(Locator('.ChatDebugView')).toBeVisible()
