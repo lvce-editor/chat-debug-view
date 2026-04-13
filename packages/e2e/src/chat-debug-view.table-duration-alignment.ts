729dae3ae040eeafed02605ac59f72142739c609
Le Vivilet <levivilet@pm.me>
2026-04-13 22:30:58 +0200

    e2e

--- packages/e2e/src/chat-debug-view.table-duration-alignment.ts
+++ packages/e2e/src/chat-debug-view.table-duration-alignment.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.table-duration-alignment'
 
+export const skip = 1
+
 export const test: Test = async ({ ChatDebug, expect, Locator }) => {
   const sessionId = 'e2e-session-table-duration-alignment'
 
