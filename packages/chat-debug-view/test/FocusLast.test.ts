b9c34a3f43a01e3ca0ff348422750dcc21397497
Le Vivilet <levivilet@pm.me>
2026-05-06 18:54:14 +0000

    fix

--- packages/chat-debug-view/test/FocusLast.test.ts
+++ packages/chat-debug-view/test/FocusLast.test.ts
@@ -54,6 +54,7 @@ test('focusLast should select the final event when another row is selected', asy
   expect(result.selectedEvent).toEqual({
     detail: 'row-2',
     eventId: 2,
+    timestamp: '2026-03-08T00:00:01.000Z',
     type: 'response',
   })
   expect(mockRpc.invocations).toEqual([['ChatStorage.loadSelectedEvent', 'session-1', 2, 'response']])
