281b1fde3396b8f30ff9615d126beae2316f4e91
Le Vivilet <levivilet@pm.me>
2026-05-06 09:29:18 +0000

    feat: Implement getHeaderCellNode function and update getHeaderCellNodes to use it

--- packages/chat-debug-view/src/parts/GetHeaderCellNodes/GetHeaderCellNodes.ts
+++ packages/chat-debug-view/src/parts/GetHeaderCellNodes/GetHeaderCellNodes.ts
@@ -1,25 +1,11 @@
-import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
-import { ChatDebugViewHeaderCell } from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as GetHeaderCellNode from '../GetHeaderCellNode/GetHeaderCellNode.ts'
 import * as TableColumn from '../TableColumn/TableColumn.ts'
-
-const getHeaderCellNode = (column: TableColumn.TableColumnName, tableColumns: readonly TableColumn.TableColumn[]): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ChatDebugViewHeaderCell,
-      name: column,
-      onClick: DomEventListenerFunctions.HandleTableHeaderClick,
-      type: VirtualDomElements.Th,
-    },
-    text(TableColumn.getTableColumnLabel(tableColumns, column)),
-  ]
-}
 
 export const getHeaderCellNodes = (
   visibleTableColumns: readonly string[],
   tableColumns: readonly TableColumn.TableColumn[] = TableColumn.createTableColumns(),
 ): readonly VirtualDomNode[] => {
   const orderedVisibleTableColumns = TableColumn.getOrderedVisibleTableColumns(visibleTableColumns, tableColumns)
-  return orderedVisibleTableColumns.flatMap((column) => getHeaderCellNode(column, tableColumns))
+  return orderedVisibleTableColumns.flatMap((column) => GetHeaderCellNode.getHeaderCellNode(column, tableColumns))
 }
