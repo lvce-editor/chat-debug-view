c3dede0053841ca0c12daebcc59c2305c4536bda
Le Vivilet <levivilet@pm.me>
2026-05-06 10:11:07 +0000

    fix

--- packages/e2e/src/chat-debug-view.table-wrapper-role.ts
+++ packages/e2e/src/chat-debug-view.table-wrapper-role.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.table-wrapper-role'
 
+export const skip = 1
+
 export const test: Test = async ({ ChatDebug, expect, Locator }) => {
   const sessionId = 'e2e-session-table-wrapper-role'
 
