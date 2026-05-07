ab8f9b9fb979ec5c017cd21f61b988191bb24f8f
Le Vivilet <levivilet@pm.me>
2026-05-07 12:51:03 +0000

    fix

--- packages/chat-debug-view/test/RenderEventListeners.test.ts
+++ packages/chat-debug-view/test/RenderEventListeners.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals'
 import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
-const getListener = (name: number) => {
+const getListener = (name: number): ReturnType<typeof RenderEventListeners.renderEventListeners>[number] | undefined => {
   const listener = RenderEventListeners.renderEventListeners().find((candidate) => candidate.name === name)
   expect(listener).toBeDefined()
   return listener
