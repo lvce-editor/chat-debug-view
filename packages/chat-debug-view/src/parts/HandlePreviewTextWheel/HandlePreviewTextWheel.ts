230d2927447481d381cd798fe97cae8e3e0977d4
Le Vivilet <levivilet@pm.me>
2026-05-10 19:48:18 +0000

    refactor: Simplify handlePreviewTextWheel by destructuring previewTextDeltaY

--- packages/chat-debug-view/src/parts/HandlePreviewTextWheel/HandlePreviewTextWheel.ts
+++ packages/chat-debug-view/src/parts/HandlePreviewTextWheel/HandlePreviewTextWheel.ts
@@ -2,5 +2,6 @@ import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
 import { setPreviewTextDeltaY } from '../PreviewVirtualization/PreviewVirtualization.ts'
 
 export const handlePreviewTextWheel = (state: ChatDebugViewState, deltaY: number): ChatDebugViewState => {
-  return setPreviewTextDeltaY(state, state.previewTextDeltaY + deltaY)
+  const { previewTextDeltaY } = state
+  return setPreviewTextDeltaY(state, previewTextDeltaY + deltaY)
 }
