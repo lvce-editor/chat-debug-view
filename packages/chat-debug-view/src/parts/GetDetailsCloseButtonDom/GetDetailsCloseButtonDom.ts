b0acbe846d79384700116b25edddb62439dc9667
Le Vivilet <levivilet@pm.me>
2026-05-12 20:16:46 +0200

    refactor: Simplify getDetailsCloseButtonDom by extracting icon definition (#273)

--- packages/chat-debug-view/src/parts/GetDetailsCloseButtonDom/GetDetailsCloseButtonDom.ts
+++ packages/chat-debug-view/src/parts/GetDetailsCloseButtonDom/GetDetailsCloseButtonDom.ts
@@ -4,24 +4,24 @@ import { ChatDebugViewDetailsClose } from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as InputName from '../InputName/InputName.ts'
 
-const detailsCloseButtonDom = [
-  {
-    'aria-label': ChatDebugStrings.closeDetails(),
-    childCount: 1,
-    className: ChatDebugViewDetailsClose,
-    name: InputName.CloseDetails,
-    onChange: DomEventListenerFunctions.HandleCloseDetails,
-    onClick: DomEventListenerFunctions.HandleCloseDetails,
-    type: VirtualDomElements.Button,
-    value: 'close',
-  },
-  {
-    childCount: 0,
-    className: 'MaskIcon MaskIconClose',
-    type: VirtualDomElements.Div,
-  },
-] as const
+const icon: VirtualDomNode = {
+  childCount: 0,
+  className: 'MaskIcon MaskIconClose',
+  type: VirtualDomElements.Div,
+}
 
 export const getDetailsCloseButtonDom = (): readonly VirtualDomNode[] => {
-  return detailsCloseButtonDom
+  return [
+    {
+      'aria-label': ChatDebugStrings.closeDetails(),
+      childCount: 1,
+      className: ChatDebugViewDetailsClose,
+      name: InputName.CloseDetails,
+      onChange: DomEventListenerFunctions.HandleCloseDetails,
+      onClick: DomEventListenerFunctions.HandleCloseDetails,
+      type: VirtualDomElements.Button,
+      value: 'close',
+    },
+    icon,
+  ]
 }
