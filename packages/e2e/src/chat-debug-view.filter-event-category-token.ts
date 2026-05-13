bd0151cc845b3c2c912b1b0b5cb9f744edc55e7c
Le Vivilet <levivilet@pm.me>
2026-05-13 21:02:55 +0000

    e2e

--- packages/e2e/src/chat-debug-view.filter-event-category-token.ts
+++ packages/e2e/src/chat-debug-view.filter-event-category-token.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.filter-event-category-token'
 
+export const skip = 1
+
 export const test: Test = async ({ ChatDebug, expect, Locator }) => {
   // arrange
   await ChatDebug.open('e2e-session-filter-token')
