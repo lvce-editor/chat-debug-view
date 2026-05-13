0fa8fb9c73880eace1d90b85713679c92e106124
Le Vivilet <levivilet@pm.me>
2026-05-13 17:34:59 +0200

    refactor: improve state destructuring in toggleTableColumnVisibility function

--- packages/chat-debug-view/src/parts/ToggleTableColumnVisibility/ToggleTableColumnVisibility.ts
+++ packages/chat-debug-view/src/parts/ToggleTableColumnVisibility/ToggleTableColumnVisibility.ts
@@ -2,12 +2,13 @@ import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
 import { isTableColumn } from '../TableColumn/TableColumn.ts'
 
 export const toggleTableColumnVisibility = (state: ChatDebugViewState, column: string): ChatDebugViewState => {
+  const { tableColumns } = state
   if (!isTableColumn(column)) {
     return state
   }
   return {
     ...state,
-    tableColumns: state.tableColumns.map((tableColumn) => {
+    tableColumns: tableColumns.map((tableColumn) => {
       if (tableColumn.name !== column) {
         return tableColumn
       }
