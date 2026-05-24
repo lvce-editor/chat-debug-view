064c48b681cefa58f273a2d75e7b71c1dc152c43
Le Vivilet <levivilet@pm.me>
2026-05-24 20:44:28 +0200

    fix

--- packages/e2e/src/chat-debug-view.render-event-rows.ts
+++ packages/e2e/src/chat-debug-view.render-event-rows.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.render-event-rows'
 
+export const skip = 1
+
 export const test: Test = async ({ ChatDebug, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
