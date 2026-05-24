b2a67f146899fada1e41b58197e0ae085ad1d6e6
Le Vivilet <levivilet@pm.me>
2026-05-24 12:10:41 +0200

    dom

--- packages/chat-debug-view/src/parts/GetRefreshButtonDom/GetRefreshButtonDom.ts
+++ packages/chat-debug-view/src/parts/GetRefreshButtonDom/GetRefreshButtonDom.ts
@@ -3,21 +3,23 @@ import * as ChatDebugStrings from '../ChatDebugStrings/ChatDebugStrings.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as InputName from '../InputName/InputName.ts'
 
+const refreshButtonDom = [
+  {
+    'aria-label': ChatDebugStrings.refreshEvents(),
+    childCount: 1,
+    className: mergeClassNames('IconButton'),
+    name: InputName.Refresh,
+    onClick: DomEventListenerFunctions.HandleClickRefresh,
+    type: VirtualDomElements.Button,
+    value: InputName.Refresh,
+  },
+  {
+    childCount: 0,
+    className: mergeClassNames(ClassNames.MaskIcon, 'MaskIconRefresh'),
+    type: VirtualDomElements.Div,
+  },
+] as const
+
 export const getRefreshButtonDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      'aria-label': ChatDebugStrings.refreshEvents(),
-      childCount: 1,
-      className: mergeClassNames('IconButton'),
-      name: InputName.Refresh,
-      onClick: DomEventListenerFunctions.HandleClickRefresh,
-      type: VirtualDomElements.Button,
-      value: InputName.Refresh,
-    },
-    {
-      childCount: 0,
-      className: mergeClassNames(ClassNames.MaskIcon, 'MaskIconRefresh'),
-      type: VirtualDomElements.Div,
-    },
-  ]
+  return refreshButtonDom
 }
