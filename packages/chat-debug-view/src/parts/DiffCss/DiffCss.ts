a815a324aae2e37897a40ed05489cabbd539b1bd
Le Vivilet <levivilet@pm.me>
2026-05-25 14:45:11 +0000

    fix

--- packages/chat-debug-view/src/parts/DiffCss/DiffCss.ts
+++ packages/chat-debug-view/src/parts/DiffCss/DiffCss.ts
@@ -5,6 +5,10 @@ export const isEqual = (oldState: ChatDebugViewState, newState: ChatDebugViewSta
     oldState.height === newState.height &&
     oldState.initial === newState.initial &&
     oldState.defaultTableWidth === newState.defaultTableWidth &&
-    oldState.tableWidth === newState.tableWidth
+    oldState.tableWidth === newState.tableWidth &&
+    oldState.tableColumns === newState.tableColumns &&
+    oldState.tableDeltaY === newState.tableDeltaY &&
+    oldState.selectedEvent === newState.selectedEvent &&
+    oldState.events.length === newState.events.length
   )
 }
