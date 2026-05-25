22a95ff7ff3edbc234857d9d5ad5c5dc098fdbb3
Le Vivilet <levivilet@pm.me>
2026-05-25 14:25:07 +0000

    feature: update getSelector function to use FocusChatDebugTable constant

--- packages/chat-debug-view/src/parts/GetRenderer/RenderFocus.ts
+++ packages/chat-debug-view/src/parts/GetRenderer/RenderFocus.ts
@@ -1,8 +1,9 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
+import { FocusChatDebugTable } from '../WhenExpression/WhenExpression.ts'
 
 const getSelector = (focus: number): string => {
-  if (focus === 0) {
+  if (focus === FocusChatDebugTable) {
     return '.Table'
   }
   return ''
