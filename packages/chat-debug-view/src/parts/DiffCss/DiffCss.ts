6f72fea2e9617c230d5f91339013b49fc086abfc
Le Vivilet <levivilet@pm.me>
2026-05-25 14:51:00 +0000

    fix

--- packages/chat-debug-view/src/parts/DiffCss/DiffCss.ts
+++ packages/chat-debug-view/src/parts/DiffCss/DiffCss.ts
@@ -2,14 +2,14 @@ import type { ChatDebugViewState } from '../State/ChatDebugViewState.ts'
 
 export const isEqual = (oldState: ChatDebugViewState, newState: ChatDebugViewState): boolean => {
   return (
+    oldState.defaultTableWidth === newState.defaultTableWidth &&
+    oldState.events.length === newState.events.length &&
     oldState.height === newState.height &&
     oldState.initial === newState.initial &&
-    oldState.defaultTableWidth === newState.defaultTableWidth &&
-    oldState.tableWidth === newState.tableWidth &&
+    oldState.selectedEvent === newState.selectedEvent &&
     oldState.tableColumns === newState.tableColumns &&
     oldState.tableDeltaY === newState.tableDeltaY &&
-    oldState.selectedEvent === newState.selectedEvent &&
-    oldState.events.length === newState.events.length &&
+    oldState.tableWidth === newState.tableWidth &&
     oldState.tableWidth === newState.tableWidth
   )
 }
