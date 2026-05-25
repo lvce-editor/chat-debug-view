0feeda523418ef8e9034174bc37ff1f2f028e40a
Le Vivilet <levivilet@pm.me>
2026-05-25 14:25:59 +0000

    feature: add RenderEditingIndex, RenderValue, and RenderSelection constants to DiffType

--- packages/chat-debug-view/src/parts/DiffType/DiffType.ts
+++ packages/chat-debug-view/src/parts/DiffType/DiffType.ts
@@ -3,3 +3,6 @@ export const RenderCss = 2
 export const RenderIncremental = 3
 export const RenderFocusContext = 4
 export const RenderFocus = 5
+export const RenderEditingIndex = 5
+export const RenderValue = 8
+export const RenderSelection = 9
