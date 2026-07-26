a850ee62d7eba9508b6a2dabad2dd015a833e8b2
Le Vivilet <levivilet@pm.me>
2026-07-26 01:46:56 +0000

    chore: migrate to npm workspaces

--- packages/build/src/config.ts
+++ packages/build/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'chat-debug-view')
 
 export const workerPath = join(root, '.tmp/dist-chat-debug-view/dist/chatDebugViewWorkerMain.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const playwrightPath = new URL('../../../node_modules/playwright/index.mjs', import.meta.url).toString()
